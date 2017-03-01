14bf0118c94bea78d771169329ac38e1efe2ebab
Unknown <ha1249@nyu.edu>
2017-02-28 23:07:13 -0500

    LR Rope + new RagDoll

---
+++ MarionetteVR/Assets/Scripts/RopeRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeRenderer : MonoBehaviour {
+
+	public Transform myTarget;
+
+	LineRenderer lr;
+
+	// Use this for initialization
+	void Start () {
+		lr = gameObject.AddComponent<LineRenderer> ();
+		lr.material = new Material (Shader.Find("Standard"));
+		lr.widthMultiplier = 0.01f;
+		lr.numPositions = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lr.SetPosition (0, transform.position);
+		lr.SetPosition (1, myTarget.position);
+		
+	}
+}
